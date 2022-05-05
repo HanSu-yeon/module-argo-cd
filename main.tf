74be1efd8c0238a41226d42e60a9c642f7e9008c
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-05-05 21:18:03 +0900

    fix ArgoCD 모듈 구성

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-#쿠버네티스 공급자 구성
+# 쿠버네티스 공급자 구성
 provider "kubernetes" {
   cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
   host                   = var.kubernetes_cluster_endpoint
@@ -8,7 +8,8 @@ provider "kubernetes" {
     args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
   }
 }
-#헬름 공급자 구성 - 쿠버네티스 배포 도구로 분산형 쿠버네티스 애플리케이션을 패키지로 배포하는데 널리 사용
+
+# 헬름 공급자 구성 - 쿠버네티스 배포 도구로 분산형 쿠버네티스 애플리케이션을 패키지로 배포하는데 널리 사용
 provider "helm" {
   kubernetes {
     cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
@@ -21,14 +22,14 @@ provider "helm" {
   }
 }
 
-#네임스페이스 생성
+# 네임스페이스 생성
 resource "kubernetes_namespace" "argo-ns" {
   metadata {
     name = "argocd"
   }
 }
 
-#헬름 공급자를 사용해 Argo CD를 설치
+# 헬름 공급자를 사용해 Argo CD를 설치
 resource "helm_release" "argocd" {
   name       = "msur"
   chart      = "argo-cd"
