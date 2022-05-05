1bb39a8d050db884806518890f80bce5d0aa34a6
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-05-05 20:40:44 +0900

    ArgoCD 모듈 구성

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-#쿠버네티스와 헬름 공급자를 구성
+#쿠버네티스 공급자 구성
 provider "kubernetes" {
   cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
   host                   = var.kubernetes_cluster_endpoint
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
     command     = "aws-iam-authenticator"
-    args        = ["toekn", "-i", "${var.kubernetes_cluster_name}"]
+    args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
   }
 }
-
+#헬름 공급자 구성 - 쿠버네티스 배포 도구로 분산형 쿠버네티스 애플리케이션을 패키지로 배포하는데 널리 사용
 provider "helm" {
   kubernetes {
     cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
@@ -16,23 +16,22 @@ provider "helm" {
     exec {
       api_version = "client.authentication.k8s.io/v1alpha1"
       command     = "aws-iam-authenticator"
-      args        = ["toekn", "-i", "${var.kubernetes_cluster_name}"]
+      args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
     }
   }
 }
 
-
+#네임스페이스 생성
 resource "kubernetes_namespace" "argo-ns" {
   metadata {
-    name = "argo"
+    name = "argocd"
   }
 }
 
+#헬름 공급자를 사용해 Argo CD를 설치
 resource "helm_release" "argocd" {
   name       = "msur"
   chart      = "argo-cd"
   repository = "https://argoproj.github.io/argo-helm"
-  namespace  = "argo"
-  version    = "2.2.5"
+  namespace  = "argocd"
 }
-
